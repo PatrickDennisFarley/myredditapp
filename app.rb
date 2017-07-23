01ce5e7eda9c96066970b03744348dd6d4fa076a
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-07-23 17:50:15 -0400

    added webpage view for deal list

--- app.rb
+++ app.rb
@@ -1,18 +1,7 @@
 require "sinatra"
 require "./reddit"
-require 'redd/middleware'
+require 'redd'
 require 'pry'
-
-use Rack::Session::Cookie
-use Redd::Middleware,
-user_agent:   'Redd:Username FashionTexter:v1.0.0 (by /u/the4oclockhero)',
-client_id:    'ylDT9JmKnj76CA',
-secret:       'IWeJkCkF821w05gekSpUg0CPtaM',
-redirect_uri: 'http://localhost:4567/auth/reddit/callback',
-scope:        %w(identity),
-via:          '/auth/reddit'
-# username:   'the4oclockhero',
-# password:   'hv3kk444'
 
 get "/" do
   redirect to("/sales")
@@ -20,20 +9,30 @@ end
 
 
 get "/sales" do
-  sales = request.env['redd.session']
+  session = Redd.it(
+    user_agent: 'Redd:FashionTexter:v1.0.0 (by /u/the4oclockhero)',
+    client_id:  'ylDT9JmKnj76CA',
+    secret:     'IWeJkCkF821w05gekSpUg0CPtaM',
+    username:   'the4oclockhero',
+    password:   'hv3kk444'
+  )
+  sales = session.subreddit('frugalmalefashion').hot
+
+  @deals = []
   sales.each do |sale|
-    "Title: #{sale.title}\n\nURL: #{sale.url}\n\n\n"
+
+    if sale.link_flair_text == "[Deal/Sale]"
+      @deals << sale
+    end
   end
-end
-
-get '/auth/reddit/callback' do
-  redirect to('/') unless request.env['redd.error']
-  "Error: #{request.env['redd.error'].message} (<a href='/'>Back</a>)"
+  erb :deals
 end
 
 
-
-
-
-# sales = reddit.subreddit('frugalmalefashion').hot
-# sales = session.subreddit('frugalmalefashion').hot
+# deals.each do |deal|
+#   binding.pry
+#   puts "Title: #{deal.title}\n\n"
+#   puts "URL: #{deal.url}\n\n"
+#   puts "Created at: #{deal.created_utc}\n\n"
+#   puts "Link Flair #{deal.link_flair_text}\n\n\n======================\n\n"
+# end
